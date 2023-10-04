6ed8c471cc5a35b2dd478eb75c6588477e409a20
Aayush Tyagi <tyagiaayush31@gmail.com>
2023-10-04 21:22:03 +0530

    First commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+console.log("This is my script")
+
+
+let result = {
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "tyagiaayush31",
+    "email": "tyagiaayush31@gmail.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "gmail.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+}
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="loading.svg">`
+    let key = "ema_live_y2BBxZltD54aX6NXPCHyQcdE3DxrNw1qwmMvUF0I"
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if (result[key] !== "" && result[key] !== " ") {
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
+
+
